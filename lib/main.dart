f4572b138734209bcf3a51ceaa02ac4c81e95ba7
tcriss <do.cristian.tr@hotmail.com>
2024-12-23 17:29:09 -0400

    feat: supabase_client

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,14 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
+import 'package:supabase_flutter/supabase_flutter.dart';
 
-void main() {
+Future<void> main() async {
+  await dotenv.load(fileName: ".env");
+  await Supabase.initialize(
+    url: dotenv.get('SB_KEY'),
+    anonKey: dotenv.get('SB_ANNON_KEY')
+  );
+  
   runApp(const MainApp());
 }
 
@@ -10,11 +18,7 @@ class MainApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return const MaterialApp(
-      home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
-        ),
-      ),
+      home: Scaffold(body: Center(child: Text('Hello World!'))),
     );
   }
 }
